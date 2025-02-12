3d0eff4ad3a188788452b312a9d94c478f71b4bd
Ramesh-T1 <104826600+Ramesh-T1@users.noreply.github.com>
2025-02-12 18:34:37 +0530

    Add files via upload

---
+++ day7/LambdaExpressionExample1.java
@@ -0,0 +1,17 @@
+package com.celcom.day7;
+
+interface Calculator {
+	int calc(int a, int b);
+}
+
+public class LambdaExpressionExample1 {
+
+	public static void main(String[] args) {
+		Calculator c = (a, b) -> a + b;
+		//c.calc(10, 20);// 30
+		System.out.println(c.calc(10, 20));
+		c = (a, b) -> a - b;
+		//c.calc(10, 20);// -10
+		System.out.println(c.calc(10, 20));
+	}
+}
---
+++ src/com/celcom/pack2/MyClass2.java
@@ -0,0 +1,10 @@
+package com.celcom.pack2;
+
+public class MyClass2 implements MyInterface {
+
+	@Override
+	public void myMethod3() {
+		System.out.println("Package 2 : My Method3");
+	}
+
+}
